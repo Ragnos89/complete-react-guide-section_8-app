5d3dfda7da73381d910fbd3c188f1d1b79ed0352
Ragnos89 <87934506+Ragnos89@users.noreply.github.com>
2021-09-05 15:32:42 +0200

    Course section 8 app update

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,11 +1,22 @@
 import React from "react";
-import "./AddUser.css";
+import styles from "./AddUser.module.css";
+import Card from "../UI/Card.js";
+import Button from "../UI/Button.js";
 const AddUser = (props) => {
   return (
-    <form>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text"></input>
-    </form>
+    <Card className={styles.input}>
+      <form>
+        <label className="input label" htmlFor="username">
+          Username
+        </label>
+        <input className="input input" id="username" type="text"></input>
+        <label className="input label" htmlFor="age">
+          Age(years)
+        </label>
+        <input className="input input" id="age" type="number"></input>
+      </form>
+      <Button type="submit">Add User</Button>
+    </Card>
   );
 };
 
