c71df4dd4ba6dad1dbb30598c6349d2cfc3d05c1
Ragnos89 <87934506+Ragnos89@users.noreply.github.com>
2021-09-07 14:28:17 +0200

    Added UserList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import AddUser from "./components/Users/AddUser";
-
+import UserList from "./components/Users/UserList";
+const dummyUsers = [
+  {
+    name: "Karol",
+    age: 32,
+  },
+];
 function App() {
   return (
     <div>
-      <AddUser></AddUser>
+      <div>
+        <AddUser></AddUser>
+      </div>
+      <div>
+        <UserList users={dummyUsers}></UserList>
+      </div>
     </div>
   );
 }
